a710a7610bfd663a370a9888f2ccf2b443418c54
msarath95 <sarath.malipeddi@tigeranalytics.com>
2020-10-27 16:06:33 +0530

    ENH: web and api prediction

--- app/app.py
+++ app/app.py
@@ -44,42 +44,40 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    if 'predict' not in session:
-        return redirect(url_for('index'))
-    form = MedianHousingFeatures()
-    if form.validate_on_submit():
+    if request.json is None:
+        if 'predict' not in session:
+            return redirect(url_for('index'))
+        form = MedianHousingFeatures()
+        if form.validate_on_submit():
+            observation = {
+                "longitude": form.longitude.data,
+                "latitude": form.latitude.data,
+                "housing_median_age": form.housing_median_age.data,
+                "total_rooms": form.total_rooms.data,
+                "total_bedrooms": form.total_bedrooms.data,
+                "population": form.population.data,
+                "households": form.households.data,
+                "median_income": form.median_income.data,
+                "ocean_proximity": form.ocean_proximity.data,
+            }
+            prediction = sr.score(score_cfg, observation)[0]
+            return render_template('prediction.html', prediction=prediction)
+        return render_template('input_features.html', form=form)
+    else:
+        if not request.json:
+            abort(400)
         observation = {
-            "longitude": form.longitude.data,
-            "latitude": form.latitude.data,
-            "housing_median_age": form.housing_median_age.data,
-            "total_rooms": form.total_rooms.data,
-            "total_bedrooms": form.total_bedrooms.data,
-            "population": form.population.data,
-            "households": form.households.data,
-            "median_income": form.median_income.data,
-            "ocean_proximity": form.ocean_proximity.data,
+            "longitude": request.json.get("longitude", ""),
+            "latitude": request.json.get("latitude", ""),
+            "housing_median_age": request.json.get("housing_median_age", ""),
+            "total_rooms": request.json.get("total_rooms", ""),
+            "total_bedrooms": request.json.get("total_bedrooms", ""),
+            "population": request.json.get("population", ""),
+            "households": request.json.get("households", ""),
+            "median_income": request.json.get("median_income", ""),
+            "ocean_proximity": request.json.get("ocean_proximity", ""),
         }
-        prediction = sr.score(score_cfg, observation)[0]
-        return render_template('prediction.html', prediction=prediction)
-    return render_template('input_features.html', form=form)
-
-
-# @app.route('/predict', methods=['GET', 'POST'])
-# def predict():
-#     if not request.json:
-#         abort(400)
-#     observation = {
-#         "longitude": request.json.get("longitude", ""),
-#         "latitude": request.json.get("latitude", ""),
-#         "housing_median_age": request.json.get("housing_median_age", ""),
-#         "total_rooms": request.json.get("total_rooms", ""),
-#         "total_bedrooms": request.json.get("total_bedrooms", ""),
-#         "population": request.json.get("population", ""),
-#         "households": request.json.get("households", ""),
-#         "median_income": request.json.get("median_income", ""),
-#         "ocean_proximity": request.json.get("ocean_proximity", ""),
-#     }
-#     return jsonify({'prediction': sr.score(score_cfg, observation)[0]}), 201
+        return jsonify({'prediction': sr.score(score_cfg, observation)[0]}), 201
 
 # curl -i -H "Content-Type: application/json" -X POST -d '{"longitude": -120.430000, "latitude": 34.870000, "housing_median_age": 21.000000, "total_rooms": 2131.000000, "total_bedrooms": 329.000000, "population": 1094.000000, "households": 353.000000, "median_income": 4.664800, "ocean_proximity": "<1H OCEAN"}' http://localhost:5000/predict
 
